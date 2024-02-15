36b9b2e0918093db5f996e93048147f4971dd691
Jeferson Barrero <jebarrero14@gmail.com>
2024-02-15 16:30:16 -0500

    Add reserve_id default value and reserve_status field***
    ***Add optional user_picture field

--- src/schemas/reserve.ts
+++ src/schemas/reserve.ts
@@ -1,9 +1,11 @@
-import { z } from "zod"
+import { z } from "astro:content"
+import { v4 as uuid } from "uuid"
 
 export const reserveSchema = z.object({
-    reserve_id: z.string().optional(),
-    reserve_date: z.string().or(z.date()).transform((v) => new Date(v)),
+    reserve_id: z.string().default(uuid()),
+    reserve_date: z.string(),
     reserve_time: z.string(),
+    reserve_status: z.enum(["active", "cancelled"]).default("active"),
     visitor_name: z.string(),
     visitor_dni: z.string().transform((v) => parseInt(v)),
     visitor_email: z.string(),
--- src/schemas/user.ts
+++ src/schemas/user.ts
@@ -3,6 +3,7 @@ import { z } from "zod"
 export const userShema = z.object({
     user_id: z.string(),
     user_role: z.enum(["admin", "client"]).default("client"),
+    user_picture: z.string().optional(),
     user_name: z.string(),
     user_email: z.string(),
 })
