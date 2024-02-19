5a81b5579ec78689984fb16bf3c1d430eae2521e
jeferson barrero <jebarrero14@gmail.com>
2024-02-19 11:09:14 -0500

    Refactor database schema and update SQL queries

--- src/db/db.sql
+++ src/db/db.sql
@@ -1,19 +1,17 @@
-CREATE TABLE reserves
-(
-    reserve_id    VARCHAR(36) PRIMARY KEY,
-    reserve_date  DATE                                   NOT NULL,
-    reserve_time  TIME                                   NOT NULL,
-    visitor_dni   INTEGER(10) CHECK ( visitor_dni >= 0 ) NOT NULL,
-    visitor_email VARCHAR(100)                           NOT NULL,
-    visitor_name  VARCHAR(100)                           NOT NULL,
-    visitor_phone VARCHAR(20)                            NOT NULL
-)
-
-CREATE TABLE users
-(
-    user_id       VARCHAR(36) PRIMARY KEY,
-    user_name    VARCHAR(225) NOT NULL,
+CREATE TABLE reserves (
+    reserve_id VARCHAR(36) PRIMARY KEY,
+    reserve_date DATE NOT NULL,
+    reserve_time TIME NOT NULL,
+    reserve_status VARCHAR(20),
+    visitor_dni INTEGER(10) NOT NULL,
+    visitor_email VARCHAR(100) NOT NULL UNIQUE,
+    visitor_name VARCHAR(100) NOT NULL,
+    visitor_phone VARCHAR(20) NOT NULL
+);
+CREATE TABLE users (
+    user_id VARCHAR(36) PRIMARY KEY,
+    user_name VARCHAR(225) NOT NULL,
     user_picture VARCHAR(225) NOT NULL,
     user_email VARCHAR(100) NOT NULL UNIQUE,
-    user_role    VARCHAR(20)  NOT NULL
-)
+    user_role VARCHAR(20) NOT NULL
+);
--- src/db/reserve.ts
+++ src/db/reserve.ts
@@ -1,62 +1,62 @@
-import { db } from "./client";
-import { reserveSchema } from "@/schemas/reserve";
-import { z } from "astro:content";
+import { db } from './client';
+import { reserveSchema } from '@/schemas/reserve';
+import { z } from 'astro:content';
 
-export async function findAllReserves(): Promise<any[]> {
-    const res = await db.execute({
-        sql: `SELECT * FROM reserves`,
-        args: [],
-    });
+export async function findAllReserves(offset: number): Promise<any[]> {
+  const res = await db.execute({
+    sql: `SELECT * FROM reserves LIMIT 10 OFFSET ?`,
+    args: [offset],
+  });
 
-    return res.rows;
+  return res.rows;
 }
 
 export async function findReserve(dni: number) {
-    const res = await db.execute({
-        sql: `SELECT visitor_dni, reserve_id FROM reserves where visitor_dni == ?`,
-        args: [dni],
-    });
+  const res = await db.execute({
+    sql: `SELECT visitor_dni, reserve_id FROM reserves where visitor_dni == ?`,
+    args: [dni],
+  });
 
-    return res.rows;
+  return res.rows;
 }
 
 export async function createReserve(data: z.infer<typeof reserveSchema>) {
-    const {
+  const {
+    reserve_id,
+    reserve_date,
+    reserve_time,
+    reserve_status,
+    visitor_dni,
+    visitor_email,
+    visitor_name,
+    visitor_phone,
+  } = data;
+
+  await db
+    .execute({
+      sql: `INSERT INTO reserves (reserve_id, reserve_time, reserve_status, visitor_name, visitor_dni, visitor_email, visitor_phone, reserve_date) VALUES (?, ?, ?, ?, ?, ?, ?, ?)`,
+      args: [
         reserve_id,
-        reserve_date,
         reserve_time,
         reserve_status,
+        visitor_name,
         visitor_dni,
         visitor_email,
-        visitor_name,
         visitor_phone,
-    } = data;
-
-    await db
-        .execute({
-            sql: `INSERT INTO reserves (reserve_id, reserve_time, reserve_status, visitor_name, visitor_dni, visitor_email, visitor_phone, reserve_date) VALUES (?, ?, ?, ?, ?, ?, ?, ?)`,
-            args: [
-                reserve_id,
-                reserve_time,
-                reserve_status,
-                visitor_name,
-                visitor_dni,
-                visitor_email,
-                visitor_phone,
-                reserve_date,
-            ],
-        })
-        .then((res) => {
-            return res.rows;
-        })
-        .catch((err) => {
-            console.error(err);
-        });
+        reserve_date,
+      ],
+    })
+    .then((res) => {
+      return res.rows;
+    })
+    .catch((err) => {
+      console.error(err);
+    });
 }
 
 export async function deleteReserve(id: string) {
-    await db.execute({
-        sql: `DELETE FROM reserves WHERE reserve_id = ?`,
-        args: [id],
-    });
+  await db.execute({
+    sql: `DELETE FROM reserves WHERE reserve_id = ?`,
+    args: [id],
+  });
 }
