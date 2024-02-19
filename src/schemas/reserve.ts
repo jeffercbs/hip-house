740fa2489aae7908623864c4ea9e6769b0564a14
jeferson barrero <jebarrero14@gmail.com>
2024-02-19 11:08:44 -0500

    Refactor schemas***

--- src/schemas/reserve.ts
+++ src/schemas/reserve.ts
@@ -1,13 +1,12 @@
-import { z } from "astro:content"
-import { v4 as uuid } from "uuid"
+import { z } from 'astro:content';
 
 export const reserveSchema = z.object({
-    reserve_id: z.string().default(uuid()),
-    reserve_date: z.string(),
-    reserve_time: z.string(),
-    reserve_status: z.enum(["active", "cancelled"]).default("active"),
-    visitor_name: z.string(),
-    visitor_dni: z.string().transform((v) => parseInt(v)),
-    visitor_email: z.string(),
-    visitor_phone: z.string(),
-})
+  reserve_id: z.string().default(''),
+  reserve_date: z.string(),
+  reserve_time: z.string(),
+  reserve_status: z.enum(['ACTIVE', 'CANCELLED']).default('ACTIVE'),
+  visitor_name: z.string(),
+  visitor_dni: z.string().transform((v) => parseInt(v)),
+  visitor_email: z.string(),
+  visitor_phone: z.string(),
+});
--- src/schemas/user.ts
+++ src/schemas/user.ts
@@ -1,9 +1,9 @@
-import { z } from "zod"
+import { z } from 'astro:content';
 
 export const userShema = z.object({
-    user_id: z.string(),
-    user_role: z.enum(["admin", "client"]).default("client"),
-    user_picture: z.string().optional(),
-    user_name: z.string(),
-    user_email: z.string(),
-})
+  user_id: z.string(),
+  user_role: z.enum(['admin', 'client']).default('client'),
+  user_picture: z.string().optional(),
+  user_name: z.string(),
+  user_email: z.string(),
+});
