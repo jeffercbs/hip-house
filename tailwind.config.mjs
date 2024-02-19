f6bb5037170b68acde669021fc55b3979c7eba7b
jeferson barrero <jebarrero14@gmail.com>
2024-02-19 11:10:16 -0500

    Update .env.dev and package.json

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,12 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
       colors: {
-        primary: "#FF6C3Aff",
+        primary: '#FF6C3Aff',
       },
     },
   },
-  plugins: [require("tailwindcss-animated")],
+  plugins: [require('tailwindcss-animated')],
 };
