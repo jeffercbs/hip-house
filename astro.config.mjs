dc6ba8e14cdcb38f06064e56d79e27ad01e23993
jeferson barrero <jebarrero14@gmail.com>
2024-02-23 14:54:09 -0500

    Update Astro configuration and dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,20 @@
-import { defineConfig } from "astro/config";
+import { defineConfig } from 'astro/config';
 // integrations
-import sitemap from "@astrojs/sitemap";
-import tailwind from "@astrojs/tailwind";
-import auth from "auth-astro";
+import tailwind from '@astrojs/tailwind';
+import auth from 'auth-astro';
 // adapter
-import vercel from "@astrojs/vercel/serverless";
-
-import svelte from "@astrojs/svelte";
+import vercel from '@astrojs/vercel/serverless';
+import svelte from '@astrojs/svelte';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://hip-house.vercel.app",
-  integrations: [sitemap(), tailwind(), auth(), svelte()],
-  output: "server",
+  site: 'https://hip-house.vercel.app',
+  integrations: [tailwind(), auth(), svelte()],
+  output: 'server',
   adapter: vercel(),
+  vite: {
+    build: {
+      cssMinify: 'lightningcss',
+    },
+  },
 });
