48350c2571e440b29a66c1929e75bf7f904aea88
jeferson barrero <jebarrero14@gmail.com>
2024-03-21 14:24:28 -0500

    Update dependencies and fix code formatting

--- auth.config.ts
+++ auth.config.ts
@@ -1,59 +1,58 @@
+import { createUser, findUser } from "@/db/user";
+import { userShema } from "@/schemas/user";
 import GoogleProvider from "@auth/core/providers/google";
-import { findUser, createUser } from "@/db/user"
+import type { TokenSet } from "@auth/core/types";
 import { defineConfig } from "auth-astro";
-import { userShema } from "@/schemas/user"
 import { v4 as uuid } from "uuid";
 
 export default defineConfig({
-    secret: import.meta.env.AUTH_SECRET,
-    providers: [
-        GoogleProvider({
-            clientId: import.meta.env.GOOGLE_CLIENT_ID,
-            clientSecret: import.meta.env.GOOGLE_CLIENT_SECRET,
-        })
-    ],
-    callbacks: {
-        async session({ session }) {
-            const user = await findUser(session?.user?.email || "")
+  secret: import.meta.env.AUTH_SECRET,
+  providers: [
+    GoogleProvider({
+      clientId: import.meta.env.GOOGLE_CLIENT_ID,
+      clientSecret: import.meta.env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+  callbacks: {
+    async session({ session }) {
+      const user = await findUser(session?.user?.email || "");
+      session.user.role = user.user_role?.toString() as any;
+      session.user.id = user.user_id?.toString() as any;
 
-            session.user.role = user.user_role?.toString() as any
-            session.user.id = user.user_id?.toString() as any
+      return session;
+    },
 
-            return session
-        },
+    async signIn({ profile }) {
+      try {
+        let user = await findUser(profile?.email || "");
 
-        async signIn({ profile }) {
-            try {
-                let user = await findUser(profile?.email || "")
+        if (!user) {
+          const new_user = userShema.parse({
+            user_id: uuid(),
+            user_email: profile?.email || "",
+            user_name: profile?.name || "",
+            user_picture: profile?.picture || "",
+          });
 
-                if (!user) {
-                    const new_user = userShema.parse({
-                        user_id: uuid(),
-                        user_email: profile?.email || "",
-                        user_name: profile?.name || "",
-                        user_picture: profile?.picture || "",
-                    })
-
-                    await createUser(new_user)
-                    return true
-                }
-                return true
-            } catch (error) {
-                return false
-            }
+          await createUser(new_user);
+          return true;
         }
-    }
+        return true;
+      } catch (error) {
+        return false;
+      }
+    },
+  },
 });
 
-
 declare module "@auth/core/types" {
-    interface Session {
-        user: {
-            name?: string
-            email?: string
-            image?: string
-            role?: string
-            id?: string
-        }
-    }
+  interface Session {
+    user: {
+      name?: string;
+      email?: string;
+      image?: string;
+      role?: string;
+      id?: string;
+    };
+  }
 }
