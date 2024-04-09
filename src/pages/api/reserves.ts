b524d775fa0d4eb80ea039e33467c5faf1a4c1f9
jeferson barrero <jebarrero14@gmail.com>
2024-04-08 22:41:08 -0500

    Refactor import statements and update database integration

--- src/pages/api/reserves.ts
+++ src/pages/api/reserves.ts
@@ -1,61 +1,67 @@
-import { createReserve, deleteReserve, findReserve } from '@/db/reserve';
-import { reserveSchema } from '@/schemas/reserve';
-import type { User } from '@/user';
-import { sendEmail } from '@/utils/send_email';
-import { format } from '@formkit/tempo';
-import type { APIRoute } from 'astro';
-import { getSession } from 'auth-astro/server';
-import { v4 as uuid } from 'uuid';
+import { reserveSchema } from "@/schemas/reserve";
+import { sendEmail } from "@/utils/send_email";
+import type { APIRoute } from "astro";
+import { Reserve, db, eq } from "astro:db";
+import { getSession } from "auth-astro/server";
+import { createHash } from "node:crypto";
 
 export const POST: APIRoute = async ({ request }) => {
   const session = await getSession(request);
 
-  const formdata = await request.formData();
-  const parsed = reserveSchema.safeParse(Object.fromEntries(formdata));
-
   if (!session) {
-    return new Response(JSON.stringify({ error: 'Primero Inicie sesión' }), {
+    return new Response(JSON.stringify({ error: "Primero Inicie sesión" }), {
       status: 401,
     });
   }
 
+  const formdata = await request.formData();
+  const parsed = reserveSchema.safeParse(Object.fromEntries(formdata));
+
   if (parsed.success) {
     try {
-      const { reserve_date, reserve_time, ...resData } = parsed.data;
-      const found = await findReserve(resData.visitor_dni);
+      const {
+        reserve_date,
+        reserve_time,
+        visitor_email,
+        visitor_name,
+        visitor_dni,
+        visitor_phone,
+      } = parsed.data;
+      const found = await db
+        .select()
+        .from(Reserve)
+        .where(eq(Reserve.visitor_email, visitor_email));
 
       if (found.length > 0) {
-        if (found[0].visitor_email === resData.visitor_email) {
-          return new Response(
-            JSON.stringify({ error: 'La reserva ya existe' }),
-            {
-              status: 400,
-            }
-          );
-        }
+        return new Response(JSON.stringify({ error: "La reserva ya existe" }), {
+          status: 400,
+        });
       }
-
-      const date = {
-        ...resData,
-        reserve_date: format(`${reserve_date}T${reserve_time}`, {
-          date: 'medium',
-          time: 'short',
-        }),
-        reserve_id: uuid(),
-      };
-
-      await createReserve(date);
-      await sendEmail(date);
+      await db.insert(Reserve).values({
+        reserve_id: createHash("sha256").update(reserve_date).digest("hex"),
+        reserve_date: new Date(reserve_date),
+        reserve_time,
+        visitor_email,
+        visitor_name,
+        visitor_dni,
+        visitor_phone,
+      });
+      await sendEmail({
+        visitor_email,
+        visitor_phone,
+        reserve_date,
+        visitor_name,
+      });
 
       return new Response(
         JSON.stringify({
           message:
-            '"¡Su reserva se ha completado con éxito! En breve, recibirá un correo electrónico con la confirmación de su reserva. ¡Muchas Gracias!"',
+            "¡Su reserva se ha completado con éxito! En breve, recibirá un correo electrónico con la confirmación de su reserva.",
         })
       );
     } catch (error) {
       return new Response(
-        JSON.stringify({ error: '¡Error interno del servidor!' }),
+        JSON.stringify({ error: "¡Error interno del servidor!" }),
         {
           status: 500,
         }
@@ -77,33 +83,32 @@ export const DELETE: APIRoute = async ({ request }) => {
   const session = await getSession(request);
 
   if (!session) {
-    return new Response(JSON.stringify({ error: 'Primero Inicie sesión' }), {
+    return new Response(JSON.stringify({ error: "Primero Inicie sesión" }), {
       status: 401,
     });
   }
 
   try {
-    if ((session.user as User).role === 'admin') {
+    if (session.user.role === "admin") {
       const url = new URL(request.url);
-      const id = url.searchParams.get('id');
+      const id = url.searchParams.get("id");
 
       if (!id) {
-        return new Response(JSON.stringify({ error: '¡ID no encontrado!' }), {
+        return new Response(JSON.stringify({ error: "¡ID no encontrado!" }), {
           status: 400,
         });
       }
 
-      await deleteReserve(id);
-
-      return new Response(JSON.stringify({ message: '¡Reserva eliminada!' }));
+      await db.delete(Reserve).where(eq(Reserve.reserve_id, id));
+      return new Response(JSON.stringify({ message: "¡Reserva eliminada!" }));
     }
 
-    return new Response(JSON.stringify({ error: 'No tienes permisos' }), {
+    return new Response(JSON.stringify({ error: "No tienes permisos" }), {
       status: 401,
     });
   } catch (error) {
     return new Response(
-      JSON.stringify({ error: '¡Error interno del servidor!' }),
+      JSON.stringify({ error: "¡Error interno del servidor!" }),
       {
         status: 500,
       }
